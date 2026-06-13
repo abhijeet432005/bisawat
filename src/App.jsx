2cf085846c7724fc50be3587c6f393927a962311
Abhijeet kumar <abhijeetkumar@Abhijeets-MacBook-Air.local>
2026-06-13 12:55:56 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-
-import Home from './pages/Home'
+import React from "react";
+import Home from "./pages/Home";
+import ReactLenis from "lenis/react";
 
 const App = () => {
   return (
-    <div className='w-full min-h-screen relative'>
+    <ReactLenis
+      root
+      options={{
+        lerp: 0.045,
+      }}
+      className="w-full min-h-screen relative"
+    >
       <Home />
-    </div>
-  )
-}
+    </ReactLenis>
+  );
+};
 
-export default App
+export default App;
